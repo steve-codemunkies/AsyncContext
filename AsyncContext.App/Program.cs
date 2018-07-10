73e79cbdf2540260344dd288380182724f9b4f98
Steven Hocking <steven.hocking@onthebeach.co.uk>
2018-07-10 08:25:27 +0100

    Created initial console application

---
+++ AsyncContext.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsyncContext.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
